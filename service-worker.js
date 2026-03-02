89c10b7303ef9d26bb072f175e06b0f85802dca1
Sea Catch Scorer <50778220+cmccreadie1@users.noreply.github.com>
2026-03-02 15:10:01 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,11 @@
-// VERSION 202 - GPS LOCATE, EDIT CATCH & PWA INSTALL
+// VERSION 202 - GPS LOCATE, EDIT CATCH & PWA INSTALL (ICON FIX)
 const CACHE_NAME = 'sea-score-v202';
 
 const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
+  './icon.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css',
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Poppins:wght@500;600;700;800&display=swap',
