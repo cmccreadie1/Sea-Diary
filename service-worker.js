69a20238b7d817017068a2743829a2a9b41af381
Sea Catch Scorer <50778220+cmccreadie1@users.noreply.github.com>
2026-03-05 20:19:51 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sea-diary-cache-v240';
+const CACHE_NAME = 'sea-diary-cache-v241';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -9,7 +9,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache v240');
+        console.log('Opened cache v241');
         return cache.addAll(urlsToCache);
       })
   );
